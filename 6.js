e41cd02903e5a2de75c8701b3a1695d834c9f18f
bephia0822 <bephia0822@gmail.com>
2020-05-10 16:26:14 +0900

    For of Loop

---
+++ 6.js
@@ -0,0 +1,48 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    total += income;
+}
+
+console.log(total);
+//204000     等於把它加起來
+//但我們這裡要學的是把它分開
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    console.log(income);
+    total += income;
+}
+//62000
+//67000
+//75000
+
+//另一個例子(有點極端ＸＤ)
+let fullName = "Dylan Coding God Israel";
+
+
+for (const char of fullName) {
+    console.log(char);
+}
+//D
+//y
+//l
+//a
+//n
+//C
+//o
+//d
+//i
+//n
+//g
+//G
+//o
+//d
+//I
+//s
+//r
+//a
+//e
+//l
